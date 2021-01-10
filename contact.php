a9040f207b67ad42c80811efcc0bf9e07c3e8dbf
anu987654321 <77241383+anu987654321@users.noreply.github.com>
2021-01-11 00:55:11 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,10 +1,45 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 
-<!-- Mirrored from themeearth.com/tf/html/carwash/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 10 Jan 2021 03:36:46 GMT -->
+<!-- Mirrored from themeearth.com/tf/html/carwash/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 10 Jan 2021 03:36:52 GMT -->
 <head>
-<?php include('inc/topheader.php'); ?>
 
+<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<meta name="description" content="CarWash - Car Wash & Car Repair HTML5 Responsive Template">
+<meta name="keywords" content="auto, auto service, Auto Wash, car, Car Repair, car service, car wash, cleaning, garage, mechanic, motor, repair, auto painting, workshop,">
+<meta name="author" content="Theme Garbage">
+
+<title>Dorto marketing Pvt. Ltd. - Car Wash</title>
+
+<link rel="icon" type="image/png" href="img/logo.jpg">
+
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="57x57" href="img/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="img/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="img/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon-180x180.png">
+
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700" rel="stylesheet">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/meanmenu.min.css">
+<link rel="stylesheet" href="css/responsive.css">
+
+
+<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
 <body>
 
 <div id="preloader">
@@ -12,9 +47,103 @@
 </div>
 
 
-<?php include('inc/header.php') ?>
-<?php include('inc/sidebar.php') ?>
+<header>
 
+<div class="hd-sec">
+<div class="container">
+<div class="row">
+<div class="col-md-4 col-sm-3">
+<div class="hd-rgt">
+<ul>
+<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#"><i class="fa fa-youtube"></i></a></li>
+<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+</ul>
+</div>
+</div>
+<div class="col-md-8 col-sm-9">
+<div class="hd-lft">
+<ul>
+<li><i class="fa fa-comment-o"></i> 24x7 live Support</li>
+<li><a href="#" class="btn appoitment-btn">Make An Appoitment</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="hd-bottom-sec">
+<div class="container">
+<div class="row">
+<div class="col-md-3 col-sm-3">
+<div class="logo">
+<a href="index-2.html"><img src="img/logo.jpg" alt="" style="height: 100px;width: 200px;margin-top: -25px;margin-bottom: -25px;" /></a>
+</div>
+</div>
+<div class="col-md-9 col-sm-9">
+<div class="contact-intro">
+<div class="info-intro">
+<i class="fa fa-envelope-o"></i>
+<div class="info-text">
+<h5>Email Address:</h5>
+<a href="mailto: Vivek5929@gmail.com"> Vivek5929@gmail.com</a>
+</div>
+</div>
+<div class="info-intro">
+<i class="fa fa fa-phone"></i>
+<div class="info-text">
+<h5>Contact Number:</h5>
+<span>+917549092305,+918092669169</span>
+</div>
+</div>
+<div class="info-intro">
+<i class="fa fa fa-clock-o"></i>
+<div class="info-text">
+<h5>Opening Time:</h5>
+<span>Sat-Sun:8:00Am-9:00PM</span>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="mnmenu-sec">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="menu">
+<nav id="main-menu" class="main-menu">
+<ul>
+<li class="active"><a href="index-2.html">Home</a></li>
+<li><a href="gallery.html">Gallery</a></li>
+<li><a href="service.html">Service<i class="fa fa-caret-down"></i></a>
+<ul>
+<li><a href="service.html">Service</a></li>
+<li><a href="service-details.html">Service Details</a></li>
+</ul>
+</li>
+<li><a href="about.html">About</a></li>
+<li><a href="employees.html">employees</a></li>
+<li><a href="blog.html">Blog<i class="fa fa-caret-down"></i></a>
+<ul>
+<li><a href="blog.html">Blog</a></li>
+<li><a href="blog-details.html">Blog Details</a></li>
+</ul>
+</li>
+<li><a href="contact.html">Contact Us</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</header>
 
 
 <div class="pagehding-sec">
@@ -127,13 +256,88 @@
 </div>
 </div>
 
+<footer>
+
+<div class="footer-sec">
+<div class="container">
+<div class="row">
+<div class="col-md-3 col-sm-6">
+<div class="footer-wedget-one">
+<h2>About Us</h2>
+<p>In this hectic world, we do not have much time to indulge in a few chores that affect our lifestyle and we tend to ignore them due to our busy schedule. Your car runs through a lot of dust and pollution every day, which makes its hygiene miserable. </p>
+<!-- <p>Lorem ipsum dolor sit amet, vel molestie nec imperdiet auctor sociosqu tellus.</p> -->
+</div>
+</div>
+<div class="col-md-3 col-sm-6">
+<div class="footer-wedget-two">
+<h2>Support links</h2>
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">Contact us</a></li>
+<li><a href="#">About Us</a></li>
+<li><a href="#">Service</a></li>
+<li><a href="#">Team</a></li>
+<li><a href="#">Blog</a></li>
+<li><a href="#">Service Details</a></li>
+</ul>
+</div>
+</div>
+<div class="col-md-3 col-sm-6">
+<div class="footer-wedget-two">
+<h2>quick links</h2>
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">Contact us</a></li>
+<li><a href="#">About Us</a></li>
+<li><a href="#">Service</a></li>
+<li><a href="#">Team</a></li>
+<li><a href="#">Blog</a></li>
+<li><a href="#">Service Details</a></li>
+</ul>
+</div>
+</div>
+<div class="col-md-3 col-sm-6">
+<div class="footer-wedget-three">
+<h2>Contact us</h2>
+<p>Dorto marketing Pvt. Ltd.-DORTO has been designed to be user-friendly and customer-centric for ease of its users to book our services.</p>
+<ul>
+<li><i class="fa fa-location-arrow"></i>Swastik apartment , Gulmohar park road , Lower Bradhwan compound Lalpur , Ranchi 834001 Jharkhand</li>
+<li><i class="fa fa-envelope-o"></i><a href= "mailto: Vivek5929@gmail.com" style="color: white;">Vivek5929@gmail.com</a><!-- <a href="https://themeearth.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="90d9fef6ffd0e9ffe5e2f4fffdf1f9febef3fffd">[email&#160;protected]</a> --></li>
+<li><i class="fa fa-phone"></i>+917549092305,+918092669169</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
 
 
-<?php include('inc/footer.php') ?>
+<div class="footer-bottom-sec">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="copy-right">
+<p>&copy; 2017 All Right Reserved.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</footer>
 
 
+<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.scrollUp.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/modernizr.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/jquery.meanmenu.min.js"></script>
+<script src="js/custom.js"></script>
 
 </body>
 
-<!-- Mirrored from themeearth.com/tf/html/carwash/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 10 Jan 2021 03:36:46 GMT -->
+<!-- Mirrored from themeearth.com/tf/html/carwash/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 10 Jan 2021 03:36:52 GMT -->
 </html>
